341048c1c5d2d67bd0396a672a4cf2de7e1f582f
rahmaxsd <132007484+rahmaxsd@users.noreply.github.com>
2026-02-16 20:01:23 +0100

    rrrrrr

--- src/main/java/ressourcesRest/ModuleRessources.java
+++ src/main/java/ressourcesRest/ModuleRessources.java
@@ -8,7 +8,6 @@ import metiers.UniteEnseignementBusiness;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("module")
@@ -16,38 +15,17 @@ public class ModuleRessources {
     private static ModuleBusiness mb = new ModuleBusiness();
     private static UniteEnseignementBusiness ueb = new UniteEnseignementBusiness();
 
-
-
-
     @POST
-    @Consumes(MediaType.APPLICATION_XML)
-    //deuxeme méthode :@Consumes("/application/xml")
-    //@Produces(MediaType.TEXT_PLAIN):on l'utilise qon on a un affichage
-    public Response ajouterModule(Module module){
-        //return ueb.addUniteEnseignement(UE);
-        if(mb.addModule(module)){
-            return Response.status(Response.Status.CREATED).entity("success").build();
-        return Response.status(Response.Status.NOT_FOUND).entity("failed").build();
-
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean ajouterModule(Module module) {
+        return mb.addModule(module);
     }
-
-@GET
-@Produces(MediaType.APPLICATION_XML)
-public Response get
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUEBySemestre(@QueryParam("semestre") String semestre,@QueryParam("codeUE") String codeUE) {
-        if(semestre != null) {
-            return Response.status(Response.Status.OK).entity(ueb.getUEBySemestre(Integer.parseInt(semestre))).build();
-
-        }
-        if(codeUE!=null){
-            return Response.status(Response.Status.OK).entity(ueb.getUEByCode(Integer.parseInt(codeUE))).build();
-        }
-        return Response.status(Response.Status.FOUND).entity(ueb.getListeUE()).build();
+    public List<Module> getAllModules() {
+        return mb.getAllModules();
     }
-
 
     @GET
     @Path("{matricule}")
